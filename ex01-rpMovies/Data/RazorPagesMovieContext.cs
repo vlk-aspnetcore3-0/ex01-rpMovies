e855d9558b23943d22690d5170f6632676787cb5
vpoteryaev <vpoteryaev@gmail.com>
2019-10-06 13:47:29 +1000

    Add the database context

---
+++ ex01-rpMovies/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex01_rpMovies.Models
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(e =>
+            {
+                e.ToTable("Movie");
+            });
+        }
+    }
+}
