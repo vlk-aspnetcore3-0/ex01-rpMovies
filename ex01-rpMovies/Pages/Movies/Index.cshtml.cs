4e2262a206ed655df78b58e9b04d674f9ecc7454
vpoteryaev <vpoteryaev@gmail.com>
2019-10-06 16:51:20 +1000

    Scaffold the Movies model
    Fix AB#8

---
+++ ex01-rpMovies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ex01_rpMovies.Models;
+
+namespace ex01_rpMovies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ex01_rpMovies.Models.RazorPagesMovieContext _context;
+
+        public IndexModel(ex01_rpMovies.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movies.ToListAsync();
+        }
+    }
+}
