4699d7b00be5a105d76d79f8e904d094af0df299
vpoteryaev <vpoteryaev@gmail.com>
2019-10-06 17:41:01 +1000

    Fix migration and update the database

---
+++ ex01-rpMovies/Migrations/20191006073855_FixMovieTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ex01_rpMovies.Migrations
+{
+    public partial class FixMovieTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Title",
+                table: "Movie",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
