cf8c96611f792e6ca5d7b7154707d4954d46d62d
vpoteryaev <vpoteryaev@gmail.com>
2019-10-20 22:43:45 +1000

    Register the context (MySql)

--- ex01-rpMovies/Startup.cs
+++ ex01-rpMovies/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,14 @@ namespace ex01_rpMovies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContextPool<Models.RazorPagesMovieContext>(
+                options => options.UseMySql(Configuration.GetConnectionString("SchoolContext"),
+                    mySqlOptions =>
+                    {
+                        mySqlOptions.ServerVersion(new Version(8, 0, 18), Pomelo.EntityFrameworkCore.MySql.Infrastructure.ServerType.MySql);
+                    }
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
