05eb2cc9abaebd016c90f68e93ab6f3bddaa0f3b
vpoteryaev <vpoteryaev@gmail.com>
2019-10-20 23:08:37 +1000

    Fixed migration and the database update

--- ex01-rpMovies/Migrations/20191020130746_fix-movie-title.Designer.cs
+++ ex01-rpMovies/Migrations/20191020130746_fix-movie-title.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ex01_rpMovies.Models;
 
 namespace ex01_rpMovies.Migrations
 {
     [DbContext(typeof(RazorPagesMovieContext))]
-    partial class RazorPagesMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20191020130746_fix-movie-title")]
+    partial class fixmovietitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
