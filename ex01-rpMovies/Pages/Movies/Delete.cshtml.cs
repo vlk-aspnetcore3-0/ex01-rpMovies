d531931d9c11b65d6981976d6ed3ad7c94ef9213
vpoteryaev <vpoteryaev@gmail.com>
2019-10-20 22:59:28 +1000

    Scaffold the movie model

---
+++ ex01-rpMovies/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ex01_rpMovies.Models;
+
+namespace ex01_rpMovies.Pages.Movies
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ex01_rpMovies.Models.RazorPagesMovieContext _context;
+
+        public DeleteModel(ex01_rpMovies.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FindAsync(id);
+
+            if (Movie != null)
+            {
+                _context.Movies.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ex01-rpMovies/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ex01_rpMovies.Models;
+
+namespace ex01_rpMovies.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ex01_rpMovies.Models.RazorPagesMovieContext _context;
+
+        public DetailsModel(ex01_rpMovies.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
